662a1defd0f41047ab5ca8c0930e550436edd0dc
Zhe Yuan <yizer16@gmail.com>
2013-12-20 19:43:51 +0800

    update test case

--- src/test/java/com/renren/zookeeper/accessor/test/PublishTest.java
+++ src/test/java/com/renren/zookeeper/accessor/test/PublishTest.java
@@ -4,12 +4,9 @@
 package com.renren.zookeeper.accessor.test;
 
 import java.io.IOException;
-import java.sql.Time;
-
 import javax.naming.OperationNotSupportedException;
 
 import org.apache.zookeeper.KeeperException;
-import org.omg.CORBA.Current;
 
 import com.renren.zookeeper.Accessor;
 import com.renren.zookeeper.ZkConfig;
@@ -26,23 +23,24 @@ public class PublishTest {
 	 */
 	public static void main(String[] args) {
 		ZkConfig config = new ZkConfig();
-		config.setHost("xcszookeepertest.n.xiaonei.com:2181");
-		config.setSessionTime(5000);
+		config.setHost("xcszookeepertest.n.xiaonei.com");
+		config.setHost("localhost");
+		config.setSessionTime(4000);
 		config.setRoot("xcs-test");
 		config.setUsername("test");
 		config.setPassword("test");
 		try {
 			Accessor accessor = Accessor.getInstance(config);
 			accessor.publishService(new Publish("test.service", "1", "0",
-					"PublishTest", "127.0.0.1"));
+					"PublishTest", null));
 			
 			Publish publish = new Publish("test.service", "1", "0",
-					"PublishTestHandle", "127.0.0.1");
+					"PublishTestHandle", "127.0.0.1".getBytes());
 			accessor.publishService(publish);
 			//publish.die();
 			while (true) {
 				System.out.println(System.currentTimeMillis() + ":" + publish.getFullPath() + "@"
-						+ publish.getValue());
+						+ new String(publish.getValue()));
 				Thread.sleep(1000);
 			}
 		} catch (InterruptedException e) {
---
+++ src/test/java/com/renren/zookeeper/accessor/test/SubscribeTest.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.renren.zookeeper.accessor.test;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.apache.zookeeper.KeeperException;
+
+import com.renren.zookeeper.Accessor;
+import com.renren.zookeeper.ZkConfig;
+import com.renren.zookeeper.accessor.Subscribe;
+
+/**
+ * @author ZheYuan
+ * 
+ */
+public class SubscribeTest {
+
+	private static class SubscribeInstance extends Subscribe {
+
+		public SubscribeInstance() {
+			super("test.service", "1", "0");
+		}
+		
+		@Override
+		public void childChanged(List<String> originList,
+				List<String> increaceList, List<String> decreaceList) {
+			System.out.println("Origin : " + originList);
+			System.out.println("Increace : " + increaceList);
+			System.out.println("Decreace : " + decreaceList);
+		}
+
+		@Override
+		public void contentChanged(String path, byte[] oldValue, byte[] newValue) {
+			System.out.println("Path = " + path + " oldvalue = " + new String(oldValue)
+					+ " newvalue = " + new String(newValue));
+		}
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SubscribeInstance subscribe = new SubscribeInstance();
+		ZkConfig config = new ZkConfig();
+		config.setHost("xcszookeepertest.n.xiaonei.com:2181");
+		config.setHost("localhost");
+		config.setSessionTime(5000);
+		config.setRoot("xcs-test");
+		config.setUsername("test");
+		config.setPassword("test");
+		try {
+			Accessor accessor = Accessor.getInstance(config);
+			accessor.subscribeService(subscribe);
+			while (true) {
+			
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OperationNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KeeperException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
